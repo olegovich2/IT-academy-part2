f4e439ad6f4505bfa6fb49f86a100a0219b9b328
olegovich2 <19922403wawa@gmail.com>
2024-09-09 21:21:51 +0300

    lesson12-HW

--- lesson12-HW/lesson12-HW.js
+++ lesson12-HW/lesson12-HW.js
@@ -6,3 +6,38 @@
 изображения должны быть из thumbnailUrl
 если изображение не может быть загружено то применить дефолтную картинку (на свой вкус)
 при нажатии на соответствующую картинку должно открыться модальное окно с полным изображением которое хранится в url (стили так же должны быть) */
+
+const getDataFromUrl = () => {
+  const url = "https://jsonplaceholder.typicode.com/photos";
+  const defaultUrl = "https://zornet.ru/_fr/21/3662993.png";
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.send();
+  xhr.onload = (dataAlbum) => {
+    dataAlbum = JSON.parse(xhr.response);
+    const body = document.querySelector("body");
+    for (const key of dataAlbum) {
+      const img = document.createElement("img");
+      img.src = key.thumbnailUrl;
+      img.title = key.title;
+      img.alt = key.title;
+      if (xhr.status !== 200) {
+        img.src = defaultUrl;
+        img.title = "Default image";
+        img.alt = "Default image";
+      }
+      body.appendChild(img);
+    }
+    actionImg();
+  };
+};
+
+function actionImg() {
+  const actionImg = document.querySelector("body");
+  actionImg.onclick = function (event) {
+    let target = event.target;
+    if (target.tagName === "BODY") return;
+    target.classList.toggle("action");
+  };
+}
+document.addEventListener("DOMContentLoaded", getDataFromUrl());
