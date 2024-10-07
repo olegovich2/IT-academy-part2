7041129d3376dc1778b71b8cf82d8bded7bd4f01
olegovich2 <19922403wawa@gmail.com>
2024-10-07 19:17:39 +0300

    movie

---
+++ src/controller.js
@@ -0,0 +1,5 @@
+import { newObject } from './listGenre.js';
+
+document.addEventListener('change', () => {
+	console.log(newObject);
+});
---
+++ src/listGenre.js
@@ -0,0 +1,66 @@
+const listGenre = {
+	featureFilm: 'Feature film',
+	shortFilm: 'Short film',
+	action: 'Action',
+	adventure: 'Adventure',
+	comedy: 'Comedy',
+	drama: 'Drama',
+	crime: 'Crime',
+	horror: 'Horror',
+	fantasy: 'Fantasy',
+	romance: 'Romance',
+	thriller: 'Thriller',
+	animation: 'Animation',
+	family: 'Family',
+	war: 'War',
+	documentary: 'Documentary',
+	musical: 'Musical',
+	biography: 'Biography',
+	sciFi: 'Sci-fi',
+	western: 'Western',
+	postApocalyptic: 'Post-apocalyptic',
+};
+
+export const newObject = {};
+
+const containerForCheckbox = document.querySelector("[data-container='checkboxesList']");
+
+const selectGenreFromObject = (object) => {
+	for (let key in object) {
+		const value = object[key];
+		createGenre(value);
+	}
+};
+
+const createGenre = (value) => {
+	const newGenre = document.createElement('div');
+	newGenre.className = 'genre_container';
+	const input = document.createElement('input');
+	input.className = 'custom-checkbox';
+	input.type = 'checkbox';
+	input.id = value;
+	const label = document.createElement('label');
+	label.className = 'genre_label';
+	label.setAttribute('for', value);
+	label.textContent = value;
+	newGenre.appendChild(input);
+	newGenre.appendChild(label);
+	containerForCheckbox.appendChild(newGenre);
+};
+
+containerForCheckbox.onclick = function (event) {
+	let target = event.target;
+	let newKey = '';
+	if (target.tagName === 'INPUT' && target.checked) {
+		newKey = JSON.stringify(target.id);
+		newObject[newKey] = target.id;
+	}
+	if (target.tagName === 'INPUT' && !target.checked) {
+		newKey = JSON.stringify(target.id);
+		delete newObject[newKey];
+	}
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	selectGenreFromObject(listGenre);
+});
